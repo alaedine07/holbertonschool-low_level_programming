2cc33fd6f4b78ce2f0a1cf6b9b8d9b678dde3c2e
alaedine07 <alaedineboufaden@gmail.com>
2021-02-03 13:17:44 +0100

    syntax update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+/**
+ * main - entry point
+ * print message without printf
+ * Return: Error
+ */
 int main(void)
 {
 	const char quote[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
