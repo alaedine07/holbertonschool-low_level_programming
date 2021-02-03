0b4f241ebc7cd5bda4bb702fbaeb3a0bec945485
alaedine07 <alaedineboufaden@gmail.com>
2021-02-03 11:58:48 +0100

    first try

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+int main(void)
+{
+	const char quote[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+
+	syscall(1, STDERR_FILENO, quote, sizeof(quote) - 1);
+	return (1);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld bytes(s)\n", sizeof(int));
+	printf("Size of float: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of double: %ld bytes(s)\n", sizeof(long long int));
+	return (0);
+}
