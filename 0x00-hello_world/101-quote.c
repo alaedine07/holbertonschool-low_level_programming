3f768b735a2ab190cd74af8de9d0228405589a6a
alaedine07 <alaedineboufaden@gmail.com>
2021-02-03 15:53:41 +0100

    C power

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	char quote[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, quote, strlen(quote));
+	write(2, quote, 0x3a);
 	return (1);
 }
