bf35521d724170beed876904e5916038544fee9c
alaedine07 <alaedineboufaden@gmail.com>
2021-02-04 13:26:20 +0100

    fix syntax

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,24 +10,22 @@
 int main(void)
 {
 	int n;
-	int abs_n;
 	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	abs_n = abs(n);
-	last_digit = abs_n % 10;
+	last_digit = n % 10;
 if (last_digit > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, n%10);
 }
 if (last_digit == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, n%10);
 }
 if ((last_digit < 6) && (last_digit != 0))
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
 }
 return (0);
 }
