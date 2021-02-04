863252a0d24d1f6828cb7259d9593aa4ec96678f
alaedine07 <alaedineboufaden@gmail.com>
2021-02-04 12:27:27 +0100

    first try

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * display message new way
+ * Return: Sucess
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		printf("%x", i);
+	}
+	putchar('\n');
+	return (0);
+}
