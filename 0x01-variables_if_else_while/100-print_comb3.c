2495afaa21c2c28d9beb437c80d464f6601290e5
alaedine07 <alaedineboufaden@gmail.com>
2021-02-04 21:52:57 +0100

    fix betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,19 +11,19 @@ int j;
 
 for (i = '0'; i < '9'; i++)
 {
-	for (j = i + 1; j <= '9'; j++)
-		{
-			if ((i > j) || (i != j))
-			{
-				putchar(i);
-				putchar(j);
-			}
-			if ((i != '8') || (j != '9'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+for (j = i + 1; j <= '9'; j++)
+{
+if ((i > j) || (i != j))
+{
+putchar(i);
+putchar(j);
+}
+if ((i != '8') || (j != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
