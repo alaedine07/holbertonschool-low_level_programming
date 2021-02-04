5d81d73eed9bcde16d048eafc4b875788e8c6421
alaedine07 <alaedineboufaden@gmail.com>
2021-02-04 13:15:04 +0100

    ascii is magic

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 15; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		printf("%x", i);
+		putchar(i);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
