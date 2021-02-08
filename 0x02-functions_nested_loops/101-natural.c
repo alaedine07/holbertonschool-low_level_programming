a47bdc5b5f334ecbf9513f87d6984f0685b14cbb
alaedine07 <alaedineboufaden@gmail.com>
2021-02-09 00:43:04 +0100

    advanced 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
-
+#include <stdio.h>
+/**
+ * main - entry point
+ * natural
+ * Return: Sucess
+ */
 int main(void)
 {
-	
+	int sum, i;
+
+	sum = 0;
+	i = 0;
+	do
+
+	{
+		if (i % 3 == 0)
+		{
+			sum = sum + i
+		}
+		if (i % 5 == 0)
+		{
+			sum = sum + i
+		}
+		i = i + 1;
+	} while (i < 1024)
 	return (0);
 }
-
