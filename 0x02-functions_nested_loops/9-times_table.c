9f64f47ef29168c9397bc4cf1a808d4d501280fb
alaedine07 <alaedineboufaden@gmail.com>
2021-02-09 11:33:33 +0100

    table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,47 +4,32 @@
  * times table
  * Return: Void
  */
-void times_table()
-{	
-	int i, j, r;
-	for (i = 0; i <= 1; i++)
+void times_table(void)
+{
+int i, j, r;
+for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			r = i * j;
-			if ((i == 0) && (j != 9))
-			{
-				_putchar('0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if ( (r < 10) && (i != 0))
+			if (r > 9)
 			{
 				_putchar(' ');
-				_putchar(r + '0');
-				_putchar(',');
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
 			}
-			else if ((r > 9) && (j != 9))
+			else
 			{
-				_putchar(r / 10 + '0');
-				_putchar(r % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+			_putchar(r + '0');
 			}
-			else if ((r > 9) && (j == 9))
-			{
-				_putchar(' ');
-				_putchar(r / 10 + '0');
-				_putchar(r % 10 + '0');
-				_putchar('\n');
-			}
-			else if ((r < 10) && (j == 9))
-			{	
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(r + '0');
-				_putchar('\n');
-			}
+			if (j != 9)
+			_putchar(',');
 		}
+		_putchar('\n');
 	}
 }
