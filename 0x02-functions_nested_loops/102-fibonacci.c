5dc08831541f0244fabcfa6d3967ab7e8e165583
alaedine07 <alaedineboufaden@gmail.com>
2021-02-09 01:26:41 +0100

    fibo 1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - print fibonacci
+ * fibonacci s
+ * Return: always 0
+*/
 int main(void)
 {
 int i;
@@ -20,11 +24,11 @@ term_3 = term_1 + term_2;
 if (i == 50)
 	printf("%ld\n", term_3);
 else
-printf("%ld, ",term_3);
+printf("%ld, ", term_3);
 
 term_1 = term_2;
 term_2 = term_3;
 
-}while ( i < 50);
+} while (i < 50);
 return (0);
 }
