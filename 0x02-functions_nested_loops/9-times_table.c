418febd80a133ff53f2f8567a60667fec4fa6b97
alaedine07 <alaedineboufaden@gmail.com>
2021-02-09 11:43:34 +0100

    betty verify

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,29 +7,29 @@
 void times_table(void)
 {
 int i, j, r;
+
 for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			r = i * j;
-			if (r > 9)
-			{
-				_putchar(' ');
-				_putchar((r / 10) + '0');
-				_putchar((r % 10) + '0');
-			}
-			else
-			{
-				if (j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-			_putchar(r + '0');
-			}
-			if (j != 9)
-			_putchar(',');
-		}
-		_putchar('\n');
+	r = i * j;
+	if (r > 9)
+	{
+	_putchar(' ');
+	_putchar((r / 10) + '0');
+	_putchar((r % 10) + '0');
+	}
+	else
+	{
+	if (j != 0)
+	{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(r + '0');
+	}
+	if (j != 9)
+	_putchar(',');
+	}
+	_putchar('\n');
 	}
 }
