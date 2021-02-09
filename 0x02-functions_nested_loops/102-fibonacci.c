5c96ae64463651a2be84f31414c1f553c3800c74
alaedine07 <alaedineboufaden@gmail.com>
2021-02-09 01:23:49 +0100

    fibo 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+int i;
+long int term_1, term_2, term_3;
+
+term_1 = 1;
+term_2 = 2;
+i = 2;
+
+printf("%ld, ", term_1);
+printf("%ld, ", term_2);
+
+do
+
+{
+i++;
+term_3 = term_1 + term_2;
+if (i == 50)
+	printf("%ld\n", term_3);
+else
+printf("%ld, ",term_3);
+
+term_1 = term_2;
+term_2 = term_3;
+
+}while ( i < 50);
+return (0);
+}
