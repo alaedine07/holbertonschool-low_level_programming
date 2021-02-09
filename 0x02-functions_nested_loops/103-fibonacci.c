33106c26f7311515d3bd85ba34ae70dbfbd0d63c
alaedine07 <alaedineboufaden@gmail.com>
2021-02-09 10:21:00 +0100

    another try

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,19 +6,16 @@
 */
 int main(void)
 {
-int i;
+
 long int term_1, term_2, term_3, sum;
 
 term_1 = 1;
 term_2 = 2;
-i = 2;
 
-
-sum = 0;
+sum = 2;
 do
 
 {
-i++;
 term_3 = term_1 + term_2;
 if (term_3 % 2 == 0)
 	sum = sum + term_3;
