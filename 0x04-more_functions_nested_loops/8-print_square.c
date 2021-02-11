6ee8f135cd48cec8239ca04492ca206fe323dabd
alaedine07 <alaedineboufaden@gmail.com>
2021-02-11 13:16:57 +0100

    # tables

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,14 +5,18 @@
  * Return: void.
  */
 void print_square(int size)
-{	int i;
+{	int i, j;
 
 	if (size > 0)
 	{
 		for (i = 0; i <= size; i++)
 		{
-			_putchar(35);
+			for (j = 0; j <= size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	
 }
