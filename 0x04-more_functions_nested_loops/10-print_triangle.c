36fcaef88162c3b4224868522e6334eb4d96714f
alaedine07 <alaedineboufaden@gmail.com>
2021-02-11 22:01:27 +0100

    triangle task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,26 +1,46 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
- * print_triangle - triangle.
- * @size: number one
- * Return: void.
+ * print_triangle - entry point
+ * @size: triangle size
+ * Return: Void
  */
 void print_triangle(int size)
-{
-	int i, j, k;
-
+{	int i, j, k;
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+	for (i = 1; i <= size; i++)
+	{	/*
+		 * if it's the last line
+		 */
+		if (i == size)
 		{
-			for (j = 0; size - i; j++)
+			for (j = 0; j < size; j++)
 			{
-				_putchar(' ');
+				printf("#");
 			}
-			for (k = 0; k <= i; k++)
-			{
-				_putchar(35);
-			}
-		_putchar('\n');
 		}
+		else
+		{	/*
+			 * trying to print the _ in this loop
+			 */
+			for (j = size; j >= i + 1; j--)
+			{
+			printf(" ");
+			}
+			/*
+			 * printing the # in this loop
+			*/
+			for (k = 1; k <= i; k++)
+			{
+			printf("#");
+			}
+		}
+	printf("\n");
+	}
+	}
+	else
+	{
+	printf("\n");
 	}
 }
