4206492e6ebaac47cf0ad23445cec6f4918935ea
alaedine07 <alaedineboufaden@gmail.com>
2021-02-11 12:25:50 +0100

    second try

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,12 @@ void print_line(int n)
 {
 	int i;
 
-	if ((n == 0) || (n < 0))
-	{
-		_putchar('\n');
-	}
-	else
-	{
-	for (i = 0; i <= n; i++)
-	{
+	if (n > 0)
+		{
+		for (i = 0; i <= n; i++)
+		{
 		_putchar(95);
-	}
+		}
+		}
 	_putchar('\n');
-	}
 }
