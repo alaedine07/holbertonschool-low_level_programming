da7f6ce738534d722b76a422f7389cd8011aa299
alaedine07 <alaedineboufaden@gmail.com>
2021-02-22 22:26:25 +0100

    printing the chess board

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,22 +12,18 @@ char *_strstr(char *haystack, char *needle)
 	int i, j = 0, k ,length_needle;
 
 	/* get the length of the needle string */
+	length_needle = 0;
 	for (i = 0; *(needle + i); i++)
 	{
 		length_needle++;
 	}
 	while (*(haystack + j))
 	{
-		/*for (k = j; *(haystack + k) && k <= length_needle && *(haystack + k) == *(needle + k); k++)
+		for (k = j; *(haystack + k) && k <= length_needle && *(haystack + k) == *(needle + k); k++)
 		{
 			haystack++;	
 		}
-		j++;*/
-		i = 0;
-		while (needle[i] == haystack[j] && haystack[j] != '\0')
-		{
-			
-		}
+		j++;
 	}
 
 	return (haystack);
