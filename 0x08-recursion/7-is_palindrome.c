1014252efb42eeb19bae5fc08da1f34c9a461a8d
alaedine07 <alaedineboufaden@gmail.com>
2021-02-25 00:59:49 +0100

    recursive palindrom

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* check - recursive string comparison
+* @str: string
+* @index: starting occurence
+* @len: string length
+* Return: int
+*/
+int check(char *str, int index, int len)
+{
+	if (*(str + index) != *(str + len))
+	{
+		return (0);
+	}
+	/* len and index are on the same char */
+	if (index == len)
+	{
+		return (1);
+	}
+	while (index < len)
+	{
+		check(str, index + 1, len - 1);
+	}
+	return (1);
+}
+/**
+* is_palindrome - entry point
+* @s: string to check
+* Return: int
+*/
+int is_palindrome(char *s)
+{
+	int n;
+	/* an empty string is a palindrome */
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	/* get the length of the string s */
+	else
+	{
+		n = 0;
+		while (*(s + n) != '\0')
+		{
+		n++;
+		}
+	}
+	return (check(s, 0, n - 1));
+}
