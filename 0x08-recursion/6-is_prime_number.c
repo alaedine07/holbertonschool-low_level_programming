064ecb5a9e807cb22420a14a860fa656c2e7acf4
alaedine07 <alaedineboufaden@gmail.com>
2021-02-24 21:40:51 +0100

    prime recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,35 @@
 #include "holberton.h"
-
-int is_prime_number(int n)
+/**
+* verif - verification
+* @n: starting number
+* @i: n divided by 2
+* Return: int
+*/
+int verif(int n, int i)
 {
-	if (n <= 1)
+	if (i == 0)
 	{
 		return (1);
 	}
-	if ( (n % (n - 1)) == 0 && (n - 1) != 1 )
+	if (n % i == 0)
 	{
 		return (0);
 	}
-	return is_prime_number(n - 1);
+	else
+	{
+		return (verif(n, i - 1));
+	}
 }
-
+/**
+* is_prime_number - entry point
+* @n: starting number
+* Return: int
+*/
+int is_prime_number(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (verif(n, n / 2));
+}
