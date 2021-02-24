95214d624fa6b9968dbeeba586661ef69c8a0546
alaedine07 <alaedineboufaden@gmail.com>
2021-02-24 21:44:01 +0100

    prime recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,7 +7,7 @@
 */
 int verif(int n, int i)
 {
-	if (i == 0)
+	if (i == 1)
 	{
 		return (1);
 	}
@@ -27,7 +27,7 @@ int verif(int n, int i)
 */
 int is_prime_number(int n)
 {
-	if (n == 1)
+	if (n <= 1)
 	{
 		return (1);
 	}
