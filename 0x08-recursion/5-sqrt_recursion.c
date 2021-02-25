7f5f03c9a8a46045680559c07e92b16104c8b8bf
alaedine07 <alaedineboufaden@gmail.com>
2021-02-25 11:59:47 +0100

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,49 @@
 #include "holberton.h"
 /**
+* search - search square root
+* @n: argument 1
+* @sqrt: argument 2
+* @temp: argument 3
+* Return: int
+*/
+int search(int n, float sqrt, float temp)
+{
+	if (sqrt == temp)
+	{
+		return (sqrt);
+	}
+	temp = sqrt;
+	return (search(n, ((n / temp + temp) / 2), temp));
+
+}
+
+
+
+
+/**
 * _sqrt_recursion - natural square root
 * @n: argument 1
 * Return: int
 */
 int _sqrt_recursion(int n)
 {
-	
+	float temp, sqrt;
+	int result;
+
+	sqrt = n / 2;
+	temp = 0;
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	result = search(n, sqrt, temp);
+	if ((result * result) != n)
+	{
+		return (-1);
+	}
+	return (result);
 }
