c9f8f7fe156f2f95c8b72a7b0c0292d61949aa22
alaedine07 <alaedineboufaden@gmail.com>
2021-02-28 15:39:06 +0100

    what's my name bro

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: the arguments
+ * Return: always success
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("the program name is: %s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+how to pass arguments to main and use them
+
+How to use __attribute__((unused)) or (void) to compile functions with unused variables or parameters 
+
+
