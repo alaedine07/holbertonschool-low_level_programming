336079fdca308dfe428367b1cb4a0b37b19c592f
alaedine07 <alaedineboufaden@gmail.com>
2021-03-02 17:15:02 +0100

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,11 +9,12 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
+	int i, j, k;
 	/**
 	 * 2-D array is actually a 1-D array where each element is a 1-D array,
 	 * so *array will point to the first value of the first 1D array
-	 * so **array will be pointing to a block of pointers which points to a 1D array
+	 * so **array will be pointing to a block of pointers
+	 * which points to a 1D array
 	 * check this: https://www.eskimo.com/~scs/cclass/int/sx9b.html
 	 */
 	int **array;
@@ -22,25 +23,25 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	else
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
 	{
-		array = malloc(height * sizeof(int *));
-		if (array == NULL)
-		{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{	/* now we fill every 1D array */
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{	/* if we get a NULL we must free the already alocated memory */
+			for (k = 0; k < i; k++)
+				free(array[k]);
+			free(array);
 			return (NULL);
 		}
-		for (i = 0; i < height; i++)
-		{	/* now we fill every 1D array */
-			array[i] = malloc(width * sizeof(int));
-			if (array[i] == NULL)
-			{
-				return (NULL);
-			}
-			for (j = 0; j < width; j++)
-			{
-				array[i][j] = 0;
-			}
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
 		}
-		return (array);
 	}
+	return (array);
 }
