3d1a0e47b2e528bb324b575c7f5948bd76e1d93a
alaedine07 <alaedineboufaden@gmail.com>
2021-03-04 20:21:52 +0100

    last mandatory

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *array_range - allocate memory and init with range
+ * @min: param1
+ * @max: param2
+ * Return: int pointer
+ **/
+int *array_range(int min, int max)
+{
+	int *p;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	p = malloc((max + 1) * sizeof(int));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= max; i++)
+	{
+		if (i >= min)
+		{
+			p[i] = i;
+		}
+	}
+
+	return (p);
+}
