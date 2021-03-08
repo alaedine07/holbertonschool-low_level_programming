c42a67c9060067ddc3207d50b8c7b422bc7b10b5
alaedine07 <alaedineboufaden@gmail.com>
2021-03-08 14:55:59 +0100

    print from pointer of strucut

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_dog - print elements of struct dog
+ * @d: pointer to structure dog
+ * Return: Void
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+		{
+			printf("owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
+}
