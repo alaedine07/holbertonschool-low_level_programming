11f0ab443f21386bc8410ab49e77aa2f29752e7a
alaedine07 <alaedineboufaden@gmail.com>
2021-03-08 23:00:48 +0100

    free dog struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,14 @@
 #include "dog.h"
 #include <stddef.h>
-#include <stdio.h>
 /**
  * free_dog - free memory dog
  * @d: pointer to structure dog
+ * if an malloc is used to set one of the elements
+ * we need to free that element
  * Return: Void
 */
 void free_dog(dog_t *d)
 {
-	/**
-	 * if an malloc is used to set one of the elements
-	 * we need to free that element
-	*/
 	if (d)
 	{
 		free(d->name);
