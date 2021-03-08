0a511062575ff276a95ce42f73da369ae555ab63
alaedine07 <alaedineboufaden@gmail.com>
2021-03-08 14:15:26 +0100

    added dog struct defenition

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog definition
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: dog structure
+ */
 struct dog
 {
 	char *name;
