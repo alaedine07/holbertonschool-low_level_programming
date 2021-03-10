67847d877e4877fbfdb6a7d1968d2ccbf8b066be
alaedine07 <alaedineboufaden@gmail.com>
2021-03-10 13:07:45 +0100

    execute call back function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "function_pointers.h"
 /**
  * print_name - execute the function being sent
  * @name: name to print
@@ -8,5 +9,4 @@
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
-	return ();
 }
