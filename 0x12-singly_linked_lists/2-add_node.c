2ca6f21d8bd20ecad08ccf662ae3a5dcddaa7f3b
alaedine07 <alaedineboufaden@gmail.com>
2021-03-19 01:20:19 +0100

    adding at the end of linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+list_t *add_node(list_t **head, const char *str)
+{
+	
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - get the string length
+ * @str: string
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * add_node_end - add a node at the end of list
+ * @head: the head of the first list
+ * @str: string to copy
+ * Return: a pointer to the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode = malloc(sizeof(list_t));
+	list_t *lastnode = *head;
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->str = strdup(str);
+	newnode->len = _strlen(str);
+	newnode->next = NULL;
+	/* is head is the last node */
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		while (lastnode->next != NULL)
+		{
+			lastnode = lastnode->next;
+		}
+		lastnode->next = newnode;
+	}
+	return (newnode);
+}
