14f002d53cfdc16d65784e187c97b4584ac49bd3
alaedine07 <alaedineboufaden@gmail.com>
2021-03-24 10:27:11 +0100

    free list with double pointers

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free a list
+ * @head: header of list
+ * Return: void
+**/
+void free_listint2(listint_t **head)
+{
+	while (*head != NULL)
+	{
+		listint_t *tmp;
+
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
+}
