513ef7bd0f4eda2f5b8fa474b369c6c795abe7af
alaedine07 <alaedineboufaden@gmail.com>
2021-03-24 11:52:28 +0100

    return node wanted index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,10 +20,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (head);
 	}
 	current_node = head;
-	while (i < index && head->next != NULL)
+	while (i < index && current_node->next != NULL)
 	{
 		current_node = current_node->next;
 		i++;
+	}
+	if (i < index)
+	{
+		return (head);
 	}
 	return (current_node);
 }
