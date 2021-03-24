2368eee6f942da762334e39db29dd67fefc1ab0b
alaedine07 <alaedineboufaden@gmail.com>
2021-03-24 11:43:42 +0100

    return node wanted index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - delete the first node
+ * @head: header of list
+ * @index: desired node
+ * Return: pointer to desired node
+**/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current_node;
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	current_node = head;
+	while (i < index)
+	{
+		current_node = current_node->next;
+		i++;
+	}
+	return (current_node);
+}
