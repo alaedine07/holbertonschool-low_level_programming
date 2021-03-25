a49675a6504921e400b941f6e5d957dd99cb0016
alaedine07 <alaedineboufaden@gmail.com>
2021-03-25 13:57:47 +0100

    binary to decimal with bitwise

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* _strlen - returns the length of a string.
+* @s: parameter
+* Return: Always 0.
+**/
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+* binary_to_uint - convert binary to decimal.
+* @b: string
+* Return: result.
+**/
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j;
+	unsigned int result = 0;
+	int len;
+
+	len = _strlen(b);
+	len = len - 1;
+	j = 0;
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = len; i >= 0; i--)
+	{
+		if (*(b + i) == '1')
+		{
+			result = result + (1 << j);
+		}
+		if (*(b + i) != '1' && *(b + i) != '0')
+		{
+			return (0);
+		}
+		j++;
+	}
+	return (result);
+}
