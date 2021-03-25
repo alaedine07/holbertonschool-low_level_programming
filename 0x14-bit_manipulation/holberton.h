c85f414613d09bfa1d1597da7aa698a8635aa1d5
alaedine07 <alaedineboufaden@gmail.com>
2021-03-25 22:57:16 +0100

    check system endianess

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -8,4 +8,5 @@ int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
+int get_endianness(void);
 #endif
