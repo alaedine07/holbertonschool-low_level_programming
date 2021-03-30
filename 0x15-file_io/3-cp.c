09793301fba45a13f1c8c2aad4595a7d5e02fcb5
alaedine07 <alaedineboufaden@gmail.com>
2021-03-30 18:37:00 +0100

    missing some checks 3rd exo

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,48 +1,81 @@
 #include "holberton.h"
 #define BUF_SIZE 1024
-
-int main(int argc, char **argv)
+/**
+ * copy_func - copies from file to file
+ * @arg_1: the destination file
+ * @arg_2: the source file
+ * Return: integer
+*/
+int copy_func(char *arg_1, char *arg_2)
 {
-	int r, file_from, file_to, w;
+	int file_from, file_to, r, w, c;
 	char buffer[BUF_SIZE];
-	/* check number of arguments */
-	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	/* if file from does not exists */
-	file_from = open(argv[1], O_RDWR);
+
+	file_from = open(arg_1, O_RDWR);
 	if (file_from < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);	
+		dprintf(2, "Error: Can't read from file %s\n", arg_1);
+		exit(98);
 	}
-	/* open or create file_to and check for error */
-	file_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	file_to = open(arg_2, O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (file_to < 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", arg_2);
 		exit(99);
 	}
-	/* read file_from and check for error */
 	r = 1024;
 	while (r == 1024)
 	{
 		r = read(file_from, buffer, 1024);
 		if (r < 0)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);	
+			dprintf(2, "Error: Can't read from file %s\n", arg_1);
+			exit(98);
 		}
 		w = write(file_to, buffer, r);
 		if (w < 0)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", arg_2);
 			exit(99);
-		}	
+		}
 	}
-	close(file_from);
-	close(file_to);
-	return (1);
+	c = close(file_from);
+	if (c < 0)
+		return (c);
+	c = close(file_to);
+	if (c < 0)
+		return (c);
+	return (0);
+}
+/**
+ * main - the main function
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: always 0
+*/
+int main(int argc, char **argv)
+{
+	int exit_code = 1;
+	/* check number of arguments */
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	exit_code = copy_func(argv[1], argv[2]);
+	switch (exit_code)
+	{
+		case (98):
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		case (99):
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		case (100):
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", exit_code);
+			exit(100);
+		default:
+			return (0);
+	}
+	return (exit_code);
 }
