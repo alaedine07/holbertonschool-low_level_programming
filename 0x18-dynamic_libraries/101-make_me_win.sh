81b0f645382d85f78f0cf7d712075ea7d61a291a
alaedine07 <alaedineboufaden@gmail.com>
2021-05-04 00:32:16 +0100

    fake puts

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 wget -P /tmp https://github.com/alaedine07/holbertonschool-low_level_programming/raw/main/0x18-dynamic_libraries/fake.so
-export LD_PRELOAD="/tmp/fake.so"
+export LD_PRELOAD=/tmp/fake.so
