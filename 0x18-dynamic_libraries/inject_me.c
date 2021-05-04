1d33c0b9fc3b189f67496c51f316179d4f1621ce
alaedine07 <alaedineboufaden@gmail.com>
2021-05-04 01:17:07 +0100

    alternative

--- 0x18-dynamic_libraries/inject_me.c
+++ 0x18-dynamic_libraries/inject_me.c
@@ -1,7 +1,17 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
 #include <stdio.h>
-/* gcc preload.c -o preload.so -fPIC -shared */
-int puts(const char *str)
+#include <string.h>
+
+/* Define an alternative name for puts */
+int (*orig_puts)(const char *str1);
+
+int puts(const char *str1) 
 {
-	printf("--> Please make me win!\n");
-	return (0);
+
+  // Backup the orginal call to strcmp() in orig_strcmp()
+  // by initialazing the pointer of orig_strcmp().
+  if(!orig_puts) orig_puts = dlsym(RTLD_NEXT, "puts");
+  // return the proper result of strcmp()
+  return orig_puts("--> Please make me win!");
 }
