e8390eca4985070f1ca11c297d22e17c630c2d06
alaedine07 <alaedineboufaden@gmail.com>
2021-05-08 18:52:24 +0100

    kernel style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,17 +7,17 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i = 0;
-    
-    if (h == NULL)
-    {
-        return (0);
-    }
-    while (h != NULL)
-    {
-        i++;
-        h = h->next;
-    }
+	size_t i = 0;
 
-    return (i);
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
 }
