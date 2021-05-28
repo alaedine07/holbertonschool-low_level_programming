5111c6978f1ecb890d4c154c93fccd5eff64181d
alaedine07 <alaedineboufaden@gmail.com>
2021-05-28 16:23:36 +0100

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,41 +5,36 @@
  * @key: The key
  * @value: The value associated with the key.
  * Return: 1 if it succeeded, 0 otherwise
-*/
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	const char *val_copy;
-	hash_node_t *new_node;
-	hash_node_t *aux;
 	unsigned long int slot;
-	/* some checks */
-	if (!key)
+	const char *val_copy;
+	hash_node_t *aux = NULL;
+	hash_node_t *new_node = NULL;
+
+	new_node = malloc(sizeof(hash_node_t *));
+	if (new_node == NULL)
 	{
 		return (0);
 	}
+	if (key == (const char *)"")
+	{
+		return (0);
+	}
+	/* get the slot */
+	slot = key_index((const unsigned char *)key, ht->size);
 	val_copy = strdup(value);
-	if (val_copy == NULL)
-	{
-		return (0);
-	}
-	/* find a slot in the hash table */
-	slot =  key_index((const unsigned char *)key, ht->size);
+	new_node->key = (char *)key;
+	new_node->value = (char *)val_copy;
 	if (ht->array[slot] == NULL)
 	{
-		/* create new node */
-		new_node = malloc(sizeof(hash_table_t *));
-		new_node->key = (char *)key;
-		new_node->value = (char *)value;
+
 		new_node->next = NULL;
-		/* add node to the table */
 		ht->array[slot] = new_node;
 	}
 	else
 	{
-		/* handle collisions */
-		new_node = malloc(sizeof(hash_table_t *));
-		new_node->key = (char *)key;
-		new_node->value = (char *)value;
 		aux = new_node;
 		new_node->next = ht->array[slot];
 		ht->array[slot] = aux;
