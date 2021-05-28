7f2106a4b8023253ee1a13787ee1db4c7da61c08
alaedine07 <alaedineboufaden@gmail.com>
2021-05-28 16:31:36 +0100

    add error condition

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *aux = NULL;
 	hash_node_t *new_node = NULL;
 
+	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
+	{
+		return (0);
+	}
 	new_node = malloc(sizeof(hash_node_t *));
 	if (new_node == NULL)
 	{
