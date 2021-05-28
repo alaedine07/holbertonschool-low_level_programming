7a1a485b21283c681c8459545d1ef3769083c1cd
alaedine07 <alaedineboufaden@gmail.com>
2021-05-28 12:23:39 +0100

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,10 +17,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
+	h_table->size = size
 	h_table->array = malloc(sizeof(hash_node_t) * size);
 	if (h_table->array == NULL)
 	{
 		free(h_table);
+		return (NULL);
 	}
 	return (h_table);
 }
