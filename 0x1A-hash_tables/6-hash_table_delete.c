d58dddd412de508e52986efd01f6a8a9aecb1e0b
alaedine07 <alaedineboufaden@gmail.com>
2021-05-30 16:05:06 +0100

    free hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node;
+	hash_node_t *tmp;
+	unsigned long int i;
+
+	if (ht)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				node = ht->array[i];
+				/* free linked list */
+				while (node != NULL)
+				{
+					tmp = node->next;
+					free(node->key);
+					free(node->value);
+					free(node);
+					node = tmp;
+				}
+			}
+		}
+	free(ht->array);
+	free(ht);
+	}
+}
