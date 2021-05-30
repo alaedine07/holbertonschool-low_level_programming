0e6020263abe5d73b81cb4c833a8b7c94991cc22
alaedine07 <alaedineboufaden@gmail.com>
2021-05-30 18:35:05 +0100

    fix allocation errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,42 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - function that adds an element to the hash table.
- * @ht: The hash table you want to add or update the key/value to.
- * @key: The key
- * @value: The value associated with the key.
- * Return: 1 if it succeeded, 0 otherwise
+ * hash_table_set - add elements to hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associeted to the key to add
+ * Return: 0(Fails) 1(Succes)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int slot;
-	const char *val_copy;
-	const char *key_copy;
-	hash_node_t *aux = NULL;
-	hash_node_t *new_node = NULL;
-	hash_node_t *curr = NULL;
+	unsigned long int slot, size;
+	hash_node_t *node; 
+	hash_node_t *curr;
 
-	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
+	if (!key || !value || !ht)
 		return (0);
-	new_node = malloc(sizeof(hash_node_t *));
-	if (new_node == NULL)
-		return (0);
-	slot = key_index((const unsigned char *)key, ht->size);
-	val_copy = strdup(value);
-	key_copy = strdup(key);
-	new_node->key = (char *)key_copy;
-	new_node->value = (char *)val_copy;
+	size = ht->size;
+	slot = key_index((const unsigned char *)key, size);
 	curr = ht->array[slot];
-	if (curr != NULL)
+	/* navigate linked list */
+	while (curr)
 	{
-		if (strcmp(curr->key, key) == 0)
+		if (curr && strcmp(key, curr->key) == 0)
 		{
 			free(curr->value);
-			curr->value = (char *)val_copy;
+			curr->value = strdup(value);
 			return (1);
 		}
+		curr = curr->next;
 	}
-	/* collision case */
-	if (ht->array[slot] == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 	{
-		new_node->next = NULL;
-		ht->array[slot] = new_node;
+		return (0);
 	}
-	else
-	{
-		aux = ht->array[slot];
-		new_node->next = aux;
-		ht->array[slot] = new_node;
-	}
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[slot];
+	ht->array[slot] = node;
 	return (1);
 }
